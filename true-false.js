54d010fb2132b4904125966442cc51f10d33fd61
riduanul <67514628+riduanul@users.noreply.github.com>
2020-08-07 07:32:28 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,11 @@
+// 0, "" empty string, undefined, null, NaN, false, = false value.
+// 4, string, '0', ' ', [], empty array, empty {} object, true = true value.
+const name = "solaiman";
+
+if (name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
