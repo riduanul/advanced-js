93ecfcd95035ac35ce3a6cf126b98cec7c187707
riduanul <67514628+riduanul@users.noreply.github.com>
2020-08-07 07:53:39 +0600

    double-vs-tripleEqual

---
+++ double-vs-tripleEqual.js
@@ -0,0 +1,11 @@
+const first = 2;
+const second = "2";
+// double equal check value and triple equal checks value and type
+if (first === second) {
+    console.log ("condition is true");
+}
+else{
+    console.log ("condition is false");
+}
+
+
